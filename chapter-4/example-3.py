38d39b5df286b50d876526597ce2f9542afecf7c
sheng chen <Chen_Sheng19@163.com>
2020-06-07 12:18:12 +0800

    example-3

--- chapter-4/example-3.py
+++ chapter-4/example-3.py
@@ -37,3 +37,63 @@ data_dir = "minist_digits_images"
 (image,label),labelnames = load_sample(data_dir)
 print(len(image),image[:2],len(label),label[:2])
 print(labelnames[label[:2]],labelnames)
+
+def get_batches(image,label,resieze_w,resize_h,channels,batch_size):
+    """
+    1. generate queue
+    2. read image and resize image
+    3. generate batch image 
+    """
+    queue = tf.train.slice_input_producer([image,label])
+    
+    label = queue[1]
+    image_c = tf.read_file(queue[0])
+    image = tf.image.decode_bmp(image_c,channels)
+    image = tf.image.resize_image_with_crop_or_pad(image,resieze_w,resize_h)
+    image = tf.image.per_image_standardization(image)
+
+    image_batch,label_batch = tf.train.batch([image,label],batch_size = batch_size,num_threads = 64)
+    image_batch = tf.cast(image_batch,tf.float32)
+
+    label_batch = tf.reshape(label_batch,[batch_size])
+    return image_batch,label_batch
+
+batch_size = 16
+image_batch,label_batch = get_batches(image,label,28,28,1,batch_size)
+
+def show_result(subplot,title,this_img):
+    p = plt.subplot(subplot)
+    p.axis('off')
+    p.imshow(np.reshape(this_img,(28,28)))
+    p.set_title(title)
+
+def show_img(index,label,img,ntop):
+    plt.figure(figsize=(20,20))
+    plt.axis('off')
+    ntop = min(ntop,9)
+    print(index)
+    for i in range(ntop):
+        show_result(100+10*ntop+1+i,label[i],img[i])
+    plt.show()
+
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess = sess,coord = coord)
+
+    try:
+        for step in np.arange(10):
+            if coord.should_stop(): 
+                break
+            images,label = sess.run([image_batch,label_batch])
+
+            show_img(step,label,images,batch_size)
+
+    except tf.errors.OutOfRangeError:
+        print("Done!")
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
